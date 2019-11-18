d528ab5984b73e3499d07c26428487e6e3f72ebd
sankar <sankar@vanuston.com>
2019-11-18 11:49:05 +0530

    salesorderlead
    salesorderlead stock checking process

--- src/app/salesorderlead/convertsales/convertsales.component.ts
+++ src/app/salesorderlead/convertsales/convertsales.component.ts
@@ -5,6 +5,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AppComponent } from 'app/app.component';
 import { dateFormatPipe } from 'app/notifications/notifications.datepipe';
 import { NotificationsComponent } from '../../notifications/notifications.component';
+import swal from 'sweetalert2';
+declare var require: any;
 
 @Component({
   selector: 'app-convertsales',
@@ -18,6 +20,7 @@ export class ConvertsalesComponent implements OnInit {
   characters: any;
   patientlist: any;
   sotypes: any;
+  products: any;
   constructor(private solead: SalesorderleadService, private fb: FormBuilder, private route: ActivatedRoute, private datepipe: dateFormatPipe, private notificationsComponent: NotificationsComponent, private router: Router) {
     this.salesorderlead = this.fb.group({
       patientid: ['', []],
@@ -28,19 +31,29 @@ export class ConvertsalesComponent implements OnInit {
       orderdate: ['', []],
       productid: ['', []],
       quantity: ['', []],
-      salesorderno:['',[]],
-      salesstatus:[1,[]],
-      totalitem:['',[]],
-      employeeid:[AppComponent.userID,[]],
-      companyrefid:[AppComponent.companyID,[]],
-      branchrefid:[AppComponent.branchID,[]],
-      locname:[AppComponent.locRefName1,[]],
-      locrefid:[AppComponent.locrefID1,[]],
+      salesorderno: ['', []],
+      salesstatus: [1, []],
+      totalitem: ['', []],
+      employeeid: [AppComponent.userID, []],
+      companyrefid: [AppComponent.companyID, []],
+      branchrefid: [AppComponent.branchID, []],
+      locname: [AppComponent.locRefName1, []],
+      locrefid: [AppComponent.locrefID1, []],
       sodetails: this.fb.array([]),
+      stockdetails: this.fb.array([])
     })
   }
+  mac: any;
+
   ngOnInit() {
+
+
     this.id = this.route.snapshot.paramMap.get('id');
+    // var MacAddress =require('get-mac-address');
+    // this.mac = MacAddress
+    //alert(MacAddress)
+    //var macaddress = require('macaddress');
+
     this.solead.fetchsaleslead(AppComponent.companyID, AppComponent.branchID, AppComponent.locRefName1, AppComponent.locrefID1, this.id).subscribe(data => {
       this.getsalesleaddata(data),
         this.solead.patientList(this.id).subscribe(data => {
@@ -53,15 +66,24 @@ export class ConvertsalesComponent implements OnInit {
         }),
         error => console.log('Errors occurs in fetchsaleslead()')
     });
-    this.solead.fetchsoleadrecord(this.id).subscribe(data => this.fetchsoleadproduct(data),
+    this.solead.fetchsoleadrecord(AppComponent.companyID, AppComponent.branchID, AppComponent.locRefName1, AppComponent.locrefID1, this.id).subscribe(data => this.fetchsoleadproduct(data),
       error => console.log('Error occurs in fetchsoleadrecord()'));
 
-      this.solead.autoIcrement(AppComponent.companyID,AppComponent.branchID,AppComponent.locrefID1,AppComponent.locRefName1).subscribe(data => {
-        this.salesorderlead.get('salesorderno').setValue(data.toString())
-      });
+    this.solead.autoIcrement(AppComponent.companyID, AppComponent.branchID, AppComponent.locrefID1, AppComponent.locRefName1).subscribe(data => {
+      this.salesorderlead.get('salesorderno').setValue(data.toString())
+    });
+    //this.getMac();
+  }
+  getMac(){
+  //   require('getmac').getMac(function(err, macAddress){
+  //     if (err)  throw err
+  //     console.log(macAddress)
+  //     alert("mac"+macAddress);
+  // })
+
   }
   getProvalues() {
-    this.solead.getprpductvalues(AppComponent.companyID, AppComponent.branchID, AppComponent.locRefName1, AppComponent.locrefID1, this.salesorderlead.get('productid').value).subscribe(
+    this.solead.getprpductvalues(AppComponent.companyID, AppComponent.branchID, AppComponent.locRefName1, AppComponent.locrefID1, this.salesorderlead.get('productid').value, this.salesorderlead.get('quantity').value).subscribe(
       data => this.getTabledata(data),
       error => console.log("Errors occurs in getprpductvalues()")
     )
@@ -91,7 +113,8 @@ export class ConvertsalesComponent implements OnInit {
     }
   }
   fetchsoleadproduct(data) {
-    const getData = <FormArray>this.salesorderlead.controls['sodetails']
+    const getData = <FormArray>this.salesorderlead.controls['sodetails'];
+    let setData = getData.value;
     if (data !== undefined || data !== null) {
       for (let p = 0; p < data.length; p++) {
         getData.push(this.fetchproduct(
@@ -99,12 +122,15 @@ export class ConvertsalesComponent implements OnInit {
           data[p][1],
           data[p][2],
           data[p][3],
-          data[p][4]
+          data[p][4],
+          data[p][5],
+          data[p][6]
         ))
+
       }
     }
   }
-  fetchproduct(pc: any, pn: any, dos: any, form: any, qty: any) {
+  fetchproduct(pc: any, pn: any, dos: any, form: any, qty: any, currentqty: any, stockst: any) {
     return this.fb.group({
 
       drugproductid: pc,
@@ -112,13 +138,17 @@ export class ConvertsalesComponent implements OnInit {
       dosagevalue: dos,
       formulationname: form,
       totalqty: qty,
+      currentqty: currentqty,
+      stockstatus: stockst,
       boxqty: 0,
       stripqty: 0,
       tabletqty: 0
+     
     })
   }
   i;
   x;
+  noproducts = [];
   getTabledata(data: any) {
     if (data !== undefined || data !== null) {
       let flag: number = 0;
@@ -138,20 +168,27 @@ export class ConvertsalesComponent implements OnInit {
             data[this.i][0],
             data[this.i][1],
             data[this.i][2],
-            data[this.i][3]
+            data[this.i][3],
+            data[this.i][4],
+            data[this.i][5]
           ));
         }
         this.characters = [];
         this.salesorderlead.get('quantity').setValue('');
       }
+      this.noproducts = setData.brandname;
+      alert(this.noproducts);
     }
+
   }
-  showBrandlist(a1: any, a2: any, a3: any, a4: any) {
+  showBrandlist(a1: any, a2: any, a3: any, a4: any, a5: any, a6: any) {
     return this.fb.group({
       drugproductid: a1,
       brandname: a2,
       formulationname: a3,
       dosagevalue: a4,
+      formalationid: a5,
+      stockstatus: a6,
       totalqty: this.salesorderlead.get('quantity').value,
       boxqty: 0,
       stripqty: 0,
@@ -175,38 +212,175 @@ export class ConvertsalesComponent implements OnInit {
     }
   }
 
-
-  onSubmit() {
+  stockvalidation(): Boolean {
     const getData = <FormArray>this.salesorderlead.controls['sodetails'];
-    let data: any = getData.value
-     this.salesorderlead.get('totalitem').setValue(data.length);
-    this.solead.savesaleslead(JSON.stringify(this.salesorderlead.value)).subscribe(
-      data => {
-        if (data == true) {
-          const getData = this.salesorderlead.controls['sodetails'];
-          this.solead.saveSaleleadRecord(JSON.stringify(getData.value)).subscribe(
-            data => {
-              if (data == true) {
-                this.notificationsComponent.addToast({ title: 'Success Message', msg: 'Data Saved Successfully.', timeout: 5000, theme: 'default', position: 'top-right', type: 'success' });
-                setTimeout(() => {
-                  this.router.navigate(['SalesOrder/ViewSalesOrder']);
-                }, 2000);
-              }
-              else {
-                this.notificationsComponent.addToast({ title: 'Error Message', msg: 'SalesOrder Not Saved', timeout: 5000, theme: 'default', position: 'top-right', type: 'error' });
-              }
-            },
-            error => console.log("Error occurs in saveSaleleadRecord()"))
-        }
-      },
-      error => console.log("Error occurs in savesaleslead()"))
+    let data: any = getData.value;
+    let setData = getData.value;
+    this.salesorderlead.get('totalitem').setValue(data.length);
+
+    for (let p = 0; p < setData.length; p++) {
+      alert(setData[p].stockstatus);
+      if (setData[p].stockstatus == 2) {
+        this.solead.getsolproducts(AppComponent.companyID, AppComponent.branchID, AppComponent.locRefName1, AppComponent.locrefID1, this.id).subscribe(data => { this.products = data });
+        this.notificationsComponent.addToast({ title: 'Error Message', msg: 'Insufficient Product Qty', timeout: 5000, theme: 'default', position: 'top-right', type: 'error' });
+        this.openBasicSwal();
+        return false;
+      }
+      else if (setData[p].stockstatus == 1) {
+        alert(setData[p].stockstatus)
+        this.solead.getsolproducts(AppComponent.companyID, AppComponent.branchID, AppComponent.locRefName1, AppComponent.locrefID1, this.id).subscribe(data => { this.products = data });
+        this.notificationsComponent.addToast({ title: 'Error Message', msg: 'Insufficient Product Qty', timeout: 5000, theme: 'default', position: 'top-right', type: 'error' });
+        this.openBasicSwal();
+        return false;
+      }
+
+    }
+    return true;
   }
-  ngAfterViewInit(){
-   //Panel Show and Hide 
-   $(document).ready(function () {
-    $("#flip").click(function () {
-      $("#panel").slideToggle("slow");
+
+  stockchecking() {
+    this.solead.stockcheck(AppComponent.companyID, AppComponent.branchID, AppComponent.locRefName1, AppComponent.locrefID1, this.salesorderlead.get('productid').value).subscribe(
+      data => {this.getstockdata(data) }
+    )
+  }
+
+  getstockdata(data) {
+    const getData = <FormArray>this.salesorderlead.controls['stockdetails'];
+    if (data != null || data != undefined) {
+      for (let p = 0; p < data.length; p++) {
+        getData.push(this.showstockdata(
+           data[p][0],
+           data[p][1],
+           data[p][2],
+           data[p][3]
+        ))
+      }
+    }
+  }
+
+  showstockdata(age: any, qty: any, shop: any,shopid:any) {
+    return this.fb.group({
+      ageingtime: age,
+      qty: qty,
+      shopname: shop,
+      tolocrefid:shopid,
+      tolocname:1,
+      reqqty:0,
+      fromlocname:AppComponent.locRefName1,
+      fromlocrefid:AppComponent.locrefID1,
+      clientcdate:AppComponent.date,
+      drugprdrefid:this.salesorderlead.get('productid').value,
+      checkbox: false,
+      companyrefid: [AppComponent.companyID, []],
+      branchrefid: [AppComponent.branchID, []],
+      locname: [AppComponent.locRefName1, []],
+      locrefid: [AppComponent.locrefID1, []],
+    })
+  }
+
+
+  savestockcheck(){
+    const getData = <FormArray>this.salesorderlead.controls['stockdetails'];
+    let data: any = getData.value;
+    this.solead.savestocking(JSON.stringify(getData.value)).subscribe(
+      data => {
+        if(data == true){
+          this.solead.saveprodstocking(JSON.stringify(getData.value)).subscribe()
+        }
+      }
+    )
+  }
+  stvalid: any;
+  onSubmit() {
+    this.stvalid = this.stockvalidation()
+    if (this.stvalid == true) {
+      this.solead.savesaleslead(JSON.stringify(this.salesorderlead.value)).subscribe(
+        data => {
+          if (data == true) {
+            const getData = this.salesorderlead.controls['sodetails'];
+            // let setData = getData.value;
+
+            this.solead.saveSaleleadRecord(JSON.stringify(getData.value)).subscribe(
+              data => {
+                if (data == true) {
+                  
+                  this.notificationsComponent.addToast({ title: 'Success Message', msg: 'Data Saved Successfully.', timeout: 5000, theme: 'default', position: 'top-right', type: 'success' });
+                  setTimeout(() => {
+                    this.router.navigate(['SalesOrder/ViewSalesOrder']);
+                  }, 2000);
+                }
+                else {
+                  this.notificationsComponent.addToast({ title: 'Error Message', msg: 'SalesOrder Not Saved', timeout: 5000, theme: 'default', position: 'top-right', type: 'error' });
+                }
+              },
+              error => console.log("Error occurs in saveSaleleadRecord()"))
+          }
+        },
+        error => console.log("Error occurs in savesaleslead()"))
+    }
+
+
+  }
+  ngAfterViewInit() {
+    //Panel Show and Hide 
+    $(document).ready(function () {
+      $("#flip").click(function () {
+        $("#panel").slideToggle("slow");
+      });
     });
-  });
+  }
+  openBasicSwal() {
+    swal({
+      title: "If You want add the Stock!",
+      text: "Click Ok",
+      showConfirmButton: true
+    }).then((openmodel) => {
+      //alert(openmodel)
+      if (openmodel) {
+        // alert(openmodel.value)
+        // this.router.navigate(['SalesOrder/ViewSalesOrder']);
+        //window.location.href ="ReorderForm/ReorderForm";
+        this.openMyModal('effect-1');
+      }
+    }).catch(swal.noop);
+  }
+
+
+  openMyModal(event) {
+    // alert("effect");
+    document.querySelector("#" + event).classList.add('md-show');
+  }
+
+
+  closeMyModal(event) {
+    ((event.target.parentElement.parentElement).parentElement).classList.remove('md-show');
+  }
+
+
 }
-}
+//  getMacAddress(){
+//   var macAddress = "";
+//   var ipAddress = "";
+//   var computerName = "";
+//   var wmi = GetObject("winmgmts:{impersonationLevel=impersonate}");
+//   e = new Enumerator(wmi.ExecQuery("SELECT * FROM Win32_NetworkAdapterConfiguration WHERE IPEnabled = True"));
+//   for(; !e.atEnd(); e.moveNext()) {
+//       var s = e.item();
+//       macAddress = s.MACAddress;
+//       ipAddress = s.IPAddress(0);
+//       computerName = s.DNSHostName;
+
+
+// }
+// function showMacAddress(){
+
+// var obj = new ActiveXObject("WbemScripting.SWbemLocator");
+
+// var s = obj.ConnectServer(".");
+
+// var properties = s.ExecQuery("SELECT * FROM Win32_NetworkAdapterConfiguration");
+
+// var e = new Enumerator (properties);
+
+// var output;
+// }
